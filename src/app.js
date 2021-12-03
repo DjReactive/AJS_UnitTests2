c898edeae21ef9e3ca5249e24102d4b2a7d842ef
Nikita <dragon768@mail.ru>
2021-12-03 17:39:21 +0500

    Added Tests on Task 2

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
-export function healthStatus (name, health = 100) {
+export function healthStatus (name, health) {
   if (health < 50) {
     if (health < 15) return 'critical';
     return 'wounded';
   }
   return 'healthy';
+}
+
+export function sortHeroes (objArray) {
+  return objArray.sort((a,b) => (a.health > b.health) ? -1 : ((b.health > a.health) ? 1 : 0));
 }
--- src/app.test.js
+++ src/app.test.js
@@ -1,5 +1,29 @@
-import {healthStatus} from './app.js';
+import {healthStatus, sortHeroes} from './app.js';
 
-test('Check Health Status', () => {
-  expect(healthStatus('маг', 90)).toBe('healthy');
+test('Статус жизней (X>50)', () => expect(healthStatus('Маг', 90)).toBe('healthy'));
+test('Статус жизней (15<X<50)', () => expect(healthStatus('Лучник', 45)).toBe('wounded'));
+test('Статус жизней (X<15)', () => expect(healthStatus('Мечник', 10)).toBe('critical'));
+
+test('Сортировка героев', () => {
+  expect(sortHeroes([
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+])).toEqual([
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+    {name: 'мечник', health: 10},
+  ]);
+});
+
+test('Сортировка героев (одинак. кол-во жизней)', () => {
+  expect(sortHeroes([
+    {name: 'маг', health: 5},
+    {name: 'мечник', health: 10},
+    {name: 'лучник', health: 5},
+])).toEqual([
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 5},
+    {name: 'лучник', health: 5},
+  ]);
 });
